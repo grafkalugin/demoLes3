8c677d000be5f995c12f6246961065596fbe1001
Калугин Александр Александрович <AKalugin2@alfabank.ru>
2023-03-07 18:59:28 +0100

    refactor SelenideElement

--- src/test/java/com/example/demoles3/demoles3/MainPage.java
+++ src/test/java/com/example/demoles3/demoles3/MainPage.java
@@ -6,28 +6,32 @@ import static com.codeborne.selenide.Selenide.$;
 
 // page_url = https://demoqa.com/automation-practice-form/
 public class MainPage {
-    public SelenideElement firstName = $("#firstName");
-    public SelenideElement lastName = $("#lastName");
-    public SelenideElement userEmail = $("#userEmail");
-    public SelenideElement sexMale = $("#genterWrapper").$(byText("Male"));
-    public SelenideElement userNumber = $("#userNumber");
-    public SelenideElement dateOfBirthInput = $("#dateOfBirthInput");
-    public SelenideElement dateOfBirthYearSelect = $(".react-datepicker__year-select");
-    public SelenideElement dateOfBirthYear1990 = $(".react-datepicker__year-select").$(byText("1990"));
-    public SelenideElement dateOfBirthMonthSelect = $(".react-datepicker__month-select");
-    public SelenideElement dateOfBirthMonthNovember = $(".react-datepicker__month-select").$(byText("November"));
-    public SelenideElement dateOfBirthDay11 = $(".react-datepicker__day--011");
-    public SelenideElement subjectsInput = $("#subjectsInput");
-    public SelenideElement hobbiesCheckboxSports = $("#hobbiesWrapper").$(byText("Sports"));
-    public SelenideElement hobbiesCheckboxReading = $("#hobbiesWrapper").$(byText("Reading"));
-    public SelenideElement hobbiesCheckboxMusic = $("#hobbiesWrapper").$(byText("Music"));
-    public SelenideElement uploadPicture = $("#uploadPicture");
-    public SelenideElement currentAddress = $("#currentAddress");
-    public SelenideElement selectState = $("#stateCity-wrapper").$(byText("Select State")); //stateCity-wrapper
-    public SelenideElement selectStateNCR = $(byText("NCR"));
-    public SelenideElement selectCity = $("#stateCity-wrapper").$(byText("Select City"));
-    public SelenideElement selectCityDelhi = $(byText("Delhi"));
-    public SelenideElement submit = $("#submit");
-    public SelenideElement modal = $(".modal-body");
-    public SelenideElement modalClose = $("#closeLargeModal");
+
+    private final String FIRST_NAME = "Aleksandr";// константа
+    public SelenideElement firstName = $("#firstName"),
+            lastName = $("#lastName"),
+            userEmail = $("#userEmail"),
+            sexMale = $("#genterWrapper").$(byText("Male")),
+            userNumber = $("#userNumber"),
+            dateOfBirthInput = $("#dateOfBirthInput"),
+            dateOfBirthYearSelect = $(".react-datepicker__year-select"),
+            dateOfBirthYear1990 = $(".react-datepicker__year-select").$(byText("1990")),
+            dateOfBirthMonthSelect = $(".react-datepicker__month-select"),
+            dateOfBirthMonthNovember = $(".react-datepicker__month-select").$(byText("November")),
+            dateOfBirthDay11 = $(".react-datepicker__day--011"),
+            subjectsInput = $("#subjectsInput"),
+            hobbiesCheckboxSports = $("#hobbiesWrapper").$(byText("Sports")),
+            hobbiesCheckboxReading = $("#hobbiesWrapper").$(byText("Reading")),
+            hobbiesCheckboxMusic = $("#hobbiesWrapper").$(byText("Music")),
+            uploadPicture = $("#uploadPicture"),
+            currentAddress = $("#currentAddress"),
+            selectState = $("#stateCity-wrapper").$(byText("Select State")), //stateCity-wrapper
+            selectStateNCR = $(byText("NCR")),
+            selectCity = $("#stateCity-wrapper").$(byText("Select City")),
+            selectCityDelhi = $(byText("Delhi")),
+            submit = $("#submit"),
+            modal = $(".modal-body"),
+            modalClose = $("#closeLargeModal");
+
+
 }
