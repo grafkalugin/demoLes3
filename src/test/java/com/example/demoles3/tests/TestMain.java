557049f02d47640e499d913b04df23454b428499
Калугин Александр Александрович <AKalugin2@alfabank.ru>
2023-04-05 15:32:45 +0200

    attach, screenshot, logs

--- src/test/java/com/example/demoles3/tests/TestMain.java
+++ src/test/java/com/example/demoles3/tests/TestMain.java
@@ -3,12 +3,14 @@ package com.example.demoles3.tests;
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.Selenide;
 import com.codeborne.selenide.logevents.SelenideLogger;
-import com.github.javafaker.Faker;
 import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
-
+import org.openqa.selenium.remote.DesiredCapabilities;
+import java.util.Map;
 import static com.codeborne.selenide.Selenide.open;
+import com.example.demoles3.helpers.Attach;
 
 public class TestMain {
 
@@ -16,16 +18,34 @@ public class TestMain {
 
     @BeforeAll
     public static void setUpAll() {
-        Configuration.browserSize = "1920x1080";
+
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setCapability("selenoid:options", Map.<String, Object>of(
+                "enableVNC", true,
+                "enableVideo", true
+        ));
+
+        Configuration.browserCapabilities = capabilities;
         Configuration.baseUrl = "https://demoqa.com";
+        Configuration.browser = "chrome";
+        Configuration.browserVersion = "100.0";
+        Configuration.browserSize = "1920x1080";
         Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
-        SelenideLogger.addListener("allure", new AllureSelenide());
     }
 
     @BeforeEach
     public void setUp() {
-        open("/automation-practice-form");
+        SelenideLogger.addListener("allure", new AllureSelenide());
         Selenide.executeJavaScript("$('#fixedban').remove()");
         Selenide.executeJavaScript("$('footer').remove()");
+        open("/automation-practice-form");
+    }
+
+    @AfterEach
+    public void attachments(){
+        Attach.screenshotAs("Last screenshot");
+        Attach.pageSource();
+        Attach.browserConsoleLogs();
+        //Attach.addVideo();
     }
 }
